ca0fd6638e6c7b2b258a125762e49a8660b81e76
John Domnic George <johndomnicgeorge@gmail.com>
2025-02-01 20:31:31 +0530

    Completed createProduct and getAllProducts, added TestContainers test

--- product-service/src/main/java/com/johndomnicgeorge/projects/product/controller/ProductController.java
+++ product-service/src/main/java/com/johndomnicgeorge/projects/product/controller/ProductController.java
@@ -1,16 +1,30 @@
 package com.johndomnicgeorge.projects.product.controller;
 
 import com.johndomnicgeorge.projects.product.dto.ProductRequest;
+import com.johndomnicgeorge.projects.product.dto.ProductResponse;
+import com.johndomnicgeorge.projects.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- product-service/src/main/java/com/johndomnicgeorge/projects/product/service/ProductService.java
+++ product-service/src/main/java/com/johndomnicgeorge/projects/product/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.johndomnicgeorge.projects.product.service;
 
 import com.johndomnicgeorge.projects.product.dto.ProductRequest;
+import com.johndomnicgeorge.projects.product.dto.ProductResponse;
 import com.johndomnicgeorge.projects.product.model.Product;
 import com.johndomnicgeorge.projects.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -16,7 +19,7 @@ public class ProductService {
     private static final Logger log = LoggerFactory.getLogger(ProductService.class);
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest) {
+    public ProductResponse createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
@@ -24,5 +27,13 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("Product created successfully");
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
     }
 }
