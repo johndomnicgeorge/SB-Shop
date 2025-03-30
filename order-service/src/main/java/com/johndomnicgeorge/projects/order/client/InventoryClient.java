11cc8a66ecf5daf15e1e0635384bc711893f4e11
John Domnic George <johndomnicgeorge@gmail.com>
2025-03-30 19:45:26 +0530

    Migrated from OpenFeign to Spring REST Clients

--- order-service/src/main/java/com/johndomnicgeorge/projects/order/client/InventoryClient.java
+++ order-service/src/main/java/com/johndomnicgeorge/projects/order/client/InventoryClient.java
@@ -1,14 +1,10 @@
 package com.johndomnicgeorge.projects.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
---
+++ order-service/src/main/java/com/johndomnicgeorge/projects/order/config/RestClientConfig.java
@@ -0,0 +1,27 @@
+package com.johndomnicgeorge.projects.order.config;
+
+import com.johndomnicgeorge.projects.order.client.InventoryClient;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}
